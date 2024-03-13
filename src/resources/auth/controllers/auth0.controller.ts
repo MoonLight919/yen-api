3e594cefac656f6d054aea76be176ca46d6fd8cb
Ihor Andriienko <42783262+MoonLight919@users.noreply.github.com>
2024-03-13 22:29:03 +0200

    feat(auth0): signup flow (#4)

--- src/resources/auth/controllers/auth0.controller.ts
+++ src/resources/auth/controllers/auth0.controller.ts
@@ -1,9 +1,38 @@
-import { UseGuards, Controller } from '@nestjs/common';
-import { ApiTags, ApiExcludeController } from '@nestjs/swagger';
+import { UseGuards, Post, Body, Controller } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiExcludeController } from '@nestjs/swagger';
+import { RestMethod } from '@modules/core/decorators';
+import { httpAssert, isError } from '@utils';
+import { AuthService } from '../services';
 import { Auth0KeyAuthenticationGuard } from '../guards';
+import { InvalidUserFromAuth0EventError } from '../errors';
+import { InvalidUserFromAuth0EventException } from '../exceptions';
+import { type Auth0EventLoginBodyDto, signupBodyDtoSchema } from '../schemas';
 
 @ApiExcludeController(true)
 @ApiTags('Auth')
 @Controller('auth')
 @UseGuards(Auth0KeyAuthenticationGuard)
-export class Auth0Controller {}
+export class Auth0Controller {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('post-registration')
+  @ApiOperation({
+    summary: 'Create user after auth0 sign up',
+  })
+  @RestMethod({
+    statusCode: 204,
+    body: signupBodyDtoSchema,
+  })
+  public async postRegistration(
+    @Body() auth0Event: Auth0EventLoginBodyDto,
+  ): Promise<void> {
+    const userOrError = await this.authService.postRegistration(auth0Event);
+    httpAssert(
+      !(userOrError instanceof InvalidUserFromAuth0EventError),
+      new InvalidUserFromAuth0EventException(
+        userOrError as InvalidUserFromAuth0EventError,
+      ),
+    );
+    httpAssert(!isError(userOrError), userOrError);
+  }
+}
--- src/resources/auth/services/auth.service.ts
+++ src/resources/auth/services/auth.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from '@resources/user/services';
+import { type UserRecord } from '@resources/user/interfaces';
+import type { Auth0EventPreUserRegistrationBodyDto } from '../schemas';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+
+  public async postRegistration(
+    auth0Event: Auth0EventPreUserRegistrationBodyDto,
+  ): Promise<UserRecord | Error | void> {
+    const existingUser = await this.userService.retrieveBy({
+      sub: auth0Event.user.user_id,
+    });
+
+    if (!existingUser) {
+      const phoneNumber = auth0Event.user.phone_number;
+      if (!phoneNumber) {
+        throw new Error('Phone number is not present');
+      }
+
+      return this.userService.create({
+        phone_number: phoneNumber,
+        sub: auth0Event.user.user_id as string,
+      });
+    }
+  }
+}
