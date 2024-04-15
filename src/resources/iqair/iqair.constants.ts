3413ea1ad5daf6d14946307575037c31976fe757
MoonLight919 <igorfujitsu@gmail.com>
2024-04-15 19:15:50 +0300

    feat(iqair): air quality current information endpoint

---
+++ src/resources/iqair/iqair.constants.ts
@@ -0,0 +1,10 @@
+export const MainPollutants: {
+  [key: string]: string;
+} = {
+  p2: 'pm2.5',
+  p1: 'pm10',
+  o3: 'Ozone O3',
+  n2: 'Nitrogen dioxide NO2',
+  s2: 'Sulfur dioxide SO2',
+  co: 'Carbon monoxide CO',
+};
