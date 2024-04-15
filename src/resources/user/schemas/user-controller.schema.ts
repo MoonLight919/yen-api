af68fc5f798c9a44e67982618d06e2cb3c07cbd1
MoonLight919 <igorfujitsu@gmail.com>
2024-04-15 18:47:02 +0300

    fix(user): expand current data patch

--- src/resources/user/schemas/user-controller.schema.ts
+++ src/resources/user/schemas/user-controller.schema.ts
@@ -4,6 +4,10 @@ export const patchUserRequestBodyDtoSchema = Type.Object(
   {
     default_region: Type.Optional(Type.String()),
     current_region: Type.Optional(Type.String()),
+    current_city: Type.Optional(Type.String()),
+    current_country: Type.Optional(Type.String()),
+    current_latitude: Type.Optional(Type.Number()),
+    current_longitude: Type.Optional(Type.Number()),
   },
   { additionalProperties: false, $id: 'PatchUserRequestBodyDto' },
 );
